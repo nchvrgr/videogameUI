e833c64cbd4f5b5f6ec8192ac34f408f187cc788
nchvrgr <nachotoasies@gmail.com>
2021-11-20 14:50:52 -0300

    Key navigation (nav and quests tab)

--- src/components/QuestList.jsx
+++ src/components/QuestList.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { useEffect } from 'react/cjs/react.development';
 
 function QuestList({quests, selected, setSelected}) {
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'ArrowDown'|| e.key === 's') {
+            setSelected(selected + 1);
+        } else if (e.key === 'ArrowUp' || e.key === 'w') {
+            setSelected(selected - 1);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+    }, [selected]);
 
     return (
         <div className="questList">
