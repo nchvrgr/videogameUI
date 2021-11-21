e8e202d3f3b2dfab9db841fdcb9cab926ee149d8
nchvrgr <nachotoasies@gmail.com>
2021-11-21 00:11:33 -0300

    let's try netlify

--- src/components/NavBarMain.jsx
+++ src/components/NavBarMain.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-// import { useEffect } from 'react/cjs/react.development'
+import { useEffect } from 'react/cjs/react.development'
 import NavBar from './NavBar'
 
 function NavBarMain({option, setOption, buttonQ, setButtonQ, buttonE, setButtonE}) {
 
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setButtonQ(false)
-    //         setButtonE(false)
-    //     }, 250);
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, [buttonQ, buttonE]);
+    useEffect(() => {
+        setTimeout(() => {
+            setButtonQ(false)
+            setButtonE(false)
+        }, 250);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [buttonQ, buttonE]);
 
     return (
         <div className="navbarMain">
