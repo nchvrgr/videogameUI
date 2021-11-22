b48c48fb7c301e8e2ace585435aaa446fb7d9d24
nchvrgr <nachotoasies@gmail.com>
2021-11-22 04:27:53 -0300

    merged styles from deploy branch

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,4 @@
-// import React, { useState, useEffect } from 'react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Menu from './Menu';
 import buttonEcs from '../media/Button_Ecs.svg';
 import buttonBack from '../media/Button_Back.svg';
@@ -8,45 +7,44 @@ function Main() {
 
     const [selectedOption, setSelectedOption] = useState("quests");
     
-    //function to add an event listener on the key Q and E pressed
-    // const [buttonQ, setButtonQ] = useState(false);
-    // const [buttonE, setButtonE] = useState(false);
+    const [buttonQ, setButtonQ] = useState(false);
+    const [buttonE, setButtonE] = useState(false);
 
-    // const handleKeyDown = (e) => {
-    //     if (e.key === 'q') {
-    //         setButtonQ(true);
-    //     }
-    //     if (e.key === 'e') {
-    //         setButtonE(true);
-    //     }
-    //     if (selectedOption === "quests") {
-    //         if (e.key === "e" || e.key === "q") {
-    //             setSelectedOption("skills");
-    //             return;
-    //         }
-    //     }
-    //     else{
-    //         if (e.key === "e" || e.key === "q") {
-    //             setSelectedOption("quests");
-    //             return "E";
-    //         }
-    //     }
-    // }
+    const handleKeyDown = (e) => {
+        if (e.key === 'q' || e.key === 'Q') {
+            setButtonQ(true);
+        }
+        if (e.key === 'e' || e.key === 'E') {
+            setButtonE(true);
+        }
+        if (selectedOption === "quests") {
+            if (e.key === "e" || e.key === "q" || e.key === "E" || e.key === "Q") {
+                setSelectedOption("skills");
+                return;
+            }
+        }
+        else{
+            if (e.key === "e" || e.key === "q" || e.key === "E" || e.key === "Q") {
+                setSelectedOption("quests");
+                return "E";
+            }
+        }
+    }
 
-    // useEffect(() => {
-    //     window.addEventListener('keydown', handleKeyDown);
-    //     return () => {
-    //         window.removeEventListener('keydown', handleKeyDown);
-    //     };
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, [selectedOption]);
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selectedOption]);
 
 
     return (
         <div className="main">
             <Menu selectedOption={selectedOption} setSelectedOption={setSelectedOption} 
-            // buttonQ={buttonQ} setButtonQ={setButtonQ}
-            // buttonE={buttonE} setButtonE={setButtonE}
+            buttonQ={buttonQ} setButtonQ={setButtonQ}
+            buttonE={buttonE} setButtonE={setButtonE}
             />
            <div className="mainButtonsBottomDiv">
                 <div className="backButtonDiv">
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
-// import React, { useState, useEffect } from 'react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+// import React, { useState } from 'react';
 import skills from '../customableStuff/skills';
 import SkillCard from './SkillCard';
 
@@ -7,25 +7,25 @@ function Skills() {
 
     const [selected, setSelected] = useState(1);
 
-    // const handleKeyDown = (e) => {
-    //     if (e.key === 'ArrowRight'|| e.key === 'd') {
-    //         if (selected < skills.length) {
-    //             setSelected(selected + 1);
-    //         }
-    //     } else if (e.key === 'ArrowLeft' || e.key === 'a') {
-    //         if (selected > 1) {
-    //             setSelected(selected - 1);
-    //         }
-    //     }
-    // }
+    const handleKeyDown = (e) => {
+        if (e.key === 'ArrowRight'|| e.key === 'd' || e.key === 'D') {
+            if (selected < skills.length) {
+                setSelected(selected + 1);
+            }
+        } else if (e.key === 'ArrowLeft' || e.key === 'a' || e.key === 'A') {
+            if (selected > 1) {
+                setSelected(selected - 1);
+            }
+        }
+    }
 
-    // useEffect(() => {
-    //     window.addEventListener('keydown', handleKeyDown);
-    //     return () => {
-    //         window.removeEventListener('keydown', handleKeyDown);
-    //     }
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, [selected]);
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selected]);
 
     return (
         <div className="skillsMain">
